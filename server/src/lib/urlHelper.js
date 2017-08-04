fdc0c9a7395c79fb85b29c1bb3a6fdeec67aabf1
Joao Paulo Ribeiro <jpnribeiro94@gmail.com>
2017-08-04 19:16:20 -0300

    fix heroku port

--- server/src/lib/urlHelper.js
+++ server/src/lib/urlHelper.js
@@ -1,9 +1,16 @@
 require('dotenv').config()
 import url from 'url'
 
-let urlServer = url.format({
-    protocol: process.env.PROTOCOL || 'http',
-    hostname: process.env.HOST || 'localhost',
-    port: process.env.PORT || 3000
-})
+let urlServer
+if (process.env.HOST.startsWith('follou')){
+  urlServer = url.format({
+      protocol: process.env.PROTOCOL,
+      hostname: process.env.HOST})
+} else {
+  urlServer = url.format({
+      protocol: process.env.PROTOCOL || 'http',
+      hostname: process.env.HOST || 'localhost',
+      port: process.env.PORT || 3000
+  })
+}
 export default urlServer
