a8611896e9de290247a811e7e9d30c502cde8d4f
Joao Paulo Ribeiro <jpnribeiro94@gmail.com>
2017-08-27 17:16:20 -0300

    add --forceExit

--- server/src/tracks/tracks.test.js
+++ server/src/tracks/tracks.test.js
@@ -11,9 +11,13 @@ beforeAll(() => {
   return redisClient.set(track.id, track.lyric_url)
 })
 
+afterAll(() => {
+  return
+})
+
 describe('Tracks API Routes', () => {
   describe('GET /api/tracks/:id', () => {
-    it('Respond with JSON', () => {
+    test('Respond with JSON', () => {
       return request(app).get('/api/tracks/7uxva9lqtvQPwTjAyAtjS0')
         .expect(200)
         .then(res => {
@@ -21,7 +25,7 @@ describe('Tracks API Routes', () => {
         })
     })
 
-    it('Respond with 404', () => {
+    test('Respond with 404', () => {
       return request(app).get('/api/tracks/404')
         .expect(404)
         .then(res => {
@@ -30,3 +34,4 @@ describe('Tracks API Routes', () => {
     })
   }) 
 })
+
